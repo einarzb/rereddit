6eb09e5cf46e45917071eebdee333bc4db1a1b11
einarzb <einarzb@gmail.com>
2017-04-12 10:28:10 +0300

    server is running, added angular ui router package

--- server.js
+++ server.js
@@ -1,3 +1,38 @@
 //todo
-//everything you need to get a server up and running
-//middleware, routes, database connection etc
+//middleware, routes
+
+var express = require('express');
+var app = express();
+
+//Serve static files in the root directory:
+app.use(express.static('public'));
+//Serve the node_modules files without the need for full path:
+app.use(express.static('node_modules'));
+
+// Mongoose for mongoDB connection:
+var mongoose = require('mongoose');
+
+//Connect to mongo database:
+mongoose.connect('mongodb://localhost/redditdb', function(err) {
+    if (err) throw err;
+});
+
+//body parser
+var bodyParser = require('body-parser')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+//Load the xample data model/schema -> Use to manipualte data from mongoDB
+var userModel = require('./models/userModel');
+var commentModel = require('./models/commentModel');
+var postModel = require('./models/postModel');
+var postModel = require('./models/postModel');
+
+// Start a server listener
+app.listen(2000, function() {
+    var appName = "rereddit";
+    console.log("App: "+ appName +" is listening on 2000. ");
+
+});
