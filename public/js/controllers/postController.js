6eac99bbd230daa46f8a2ba906dde5008322340f
einarzb <einarzb@gmail.com>
2017-04-12 21:49:58 +0300

    added request npm package to set recaptcha server side work

--- public/js/controllers/postController.js
+++ public/js/controllers/postController.js
@@ -1,4 +1,4 @@
-app.controller('PostController', function(vcRecaptchaService, $http, $scope, postFactory) {
+app.controller('PostController', function(vcRecaptchaService, $scope, postFactory) {
 $scope.title;
 $scope.sub;
 
@@ -10,12 +10,6 @@ var vm = this;
 			alert("Please resolve the captcha and submit!")
 		} else {
       alert(vcRecaptchaService.getResponse());
-      var post_data = {
-        'title':vm.title,
-        'text':vm.text,
-        'link':vm.link,
-        'g-recaptcha-response':vcRecaptchaService.getResponse() //send g-captcah-reponse to our server
-      }
 		}
 	};
 
