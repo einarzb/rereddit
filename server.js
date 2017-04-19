490c0049946348717d284a019760608f8fb11171
einarzb <einarzb@gmail.com>
2017-04-19 15:38:05 +0300

    commented shit

--- server.js
+++ server.js
@@ -160,28 +160,27 @@ app.delete('/posts/:id/comments/:commentID', function(req,res){
       console.error(err)
       return next(err);
     }else{
-      var
-      console.log('removed comment', commentRemoved);
+      console.log(commentRemoved);
       res.send(commentRemoved);
     }
   });
-});
-  Post.findOne({_id: req.params.id}, function(err, foundPost){
-    if (err){
-        console.error(err)
-        return next(err);
-    } else {
-        var deletedComment = new Comment(req.body);
-        console.log(deletedComment); //output id of comment!
-        //foundPost.comments is the array of comments of this.post
-        //deletedComment id is being deleted to the comments array
-        foundPost.comments.splice(deletedComment, 1);
-        //id is being saved
-        deletedComment.save();
-        //full post object with array comments is being saved
-        foundPost.save();
-        res.json(foundPost);
-    }
+// });
+//   Post.findOne({_id: req.params.id}, function(err, foundPost){
+//     if (err){
+//         console.error(err)
+//         return next(err);
+//     } else {
+//         var deletedComment = new Comment(req.body);
+//         console.log(deletedComment); //output id of comment!
+//         //foundPost.comments is the array of comments of this.post
+//         //deletedComment id is being deleted to the comments array
+//         foundPost.comments.splice(deletedComment, 1);
+//         //id is being saved
+//         deletedComment.save();
+//         //full post object with array comments is being saved
+//         foundPost.save();
+//         res.json(foundPost);
+//     }
   })
 
 
